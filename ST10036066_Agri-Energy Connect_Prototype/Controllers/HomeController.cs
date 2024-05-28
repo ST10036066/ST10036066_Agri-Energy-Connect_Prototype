dd1ddfeb10908b39a90f7b716982bbf1ff1f87a7
Azania Aria Ncube <ST10036066@vcconnect.edu.za>
2024-05-28 14:21:12 +0200

    Add project files.

---
+++ ST10036066_Agri-Energy Connect_Prototype/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ST10036066_Agri_Energy_Connect_Prototype.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult GreenEnergyMarketplace()
+        {
+            ViewBag.Message = "Green Energy Makrtplace";
+
+            return View();
+        }
+    }
+}
---
+++ ST10036066_Agri-Energy Connect_Prototype/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ST10036066_Agri_Energy_Connect_Prototype.MvcApplication" Language="C#" %>
